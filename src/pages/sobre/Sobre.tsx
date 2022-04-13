b3fce3a8af5e40129ce204deb3ad43644ad81aa7
Luís Fernando da Silveira <63754409+Louissilver@users.noreply.github.com>
2022-04-12 21:31:03 -0300

    Ajuste em erro causado por merge

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -4,6 +4,13 @@ import { LayoutBaseDePagina } from '../../shared/layouts/LayoutBaseDePagina';
 
 const secoes = [
   {
+    titulo: 'Nossa história e propósito',
+    texto: ` A CAP foi fundada pelo Maike Terres e Lucas Armando em 2019, teve origem a partir de uma vontade imensa de criar um ambiente próspero onde tivessem oportunidades para todos que estivessem dispostos a crescer, tendo como objetivo principal o desenvolvimento de pessoas para se tornarem excelentes corretores de imóveis, transformando a visão do mercado sobre esse profissional.
+    Desde o início da empresa trabalhamos com VENDA de imóveis, hoje possuímos também um projeto de marketing digital que permite o desenvolvimento da empresa através do online em paralelo ao físico.
+    
+    A Equipe da CAP é formada por aproximadamente 45 pessoas, sendo parte administrativa e parte comercial.
+    Possuímos hoje imóveis em 15 cidades, somando mais de 100 empreendimentos ativos.
+    `,
     imagem: undefined,
     botao: undefined,
     textAlign: 'justify',
